df46ab52b70f98cb169249480756a0eb7c438c70
ty要nx <ttyy373172968@163.com>
2021-04-11 21:09:04 +0800

    修复页面没有动画的bug

--- dist/css.16d824bf.js
+++ dist/css.16d824bf.js
@@ -127,81 +127,7 @@ exports.default = void 0;
 var string = ".skin * {\n    box-sizing: border-box;\n    margin: 0;\n    padding: 0;\n  }\n  .skin *::before,\n  .skin *::after {\n    box-sizing: border-box;\n  }\n \n  .skin {\n    background-color: #ffe600;\n    min-height: 50vh;\n    position: relative;\n  }\n  .nose {\n    position: relative;\n    border: 10px solid #000;\n    border-color: #000 transparent transparent transparent;\n    border-bottom: none;\n    width: 0px;\n    height: 0px;\n    left: 50%;\n    top: 145px;\n    margin-left: -10px;\n    z-index: 10;\n  }\n  @keyframes wave {\n    0% {\n      transform: rotate(0deg);\n    }\n    33% {\n      transform: rotate(5deg);\n    }\n    66% {\n      transform: rotate(-5deg);\n    }\n    100% {\n      transform: rotate(0deg);\n    }\n  }\n  .nose:hover {\n    transform-origin: center bottom;\n    animation: wave 200ms infinite linear;\n  }\n  .yuan {\n    position: absolute;\n    border: 1px solid #000;\n    width: 20px;\n    height: 6px;\n    top: -16px;\n    left: -10px;\n    border-radius: 10px 10px 0 0;\n    background-color: #000;\n  }\n  \n  .eyes {\n    position: absolute;\n    border: 2px solid #000;\n    width: 64px;\n    height: 64px;\n    left: 50%;\n    top: 100px;\n    margin-left: -32px;\n    background-color: #2e2e2e;\n    border-radius: 50%;\n  }\n  .eyes::before {\n    content: \"\";\n    display: block;\n    border: 3px solid #000;\n    width: 25px;\n    height: 25px;\n    background-color: #fff;\n    border-radius: 50%;\n    position: relative;\n    left: 8px;\n    top: 4px;\n  }\n  .eyes.left {\n    transform: translateX(-100px);\n  }\n  .eyes.right {\n    transform: translateX(100px);\n  }\n  .mouth {\n    width: 200px;\n    height: 200px;\n    position: absolute;\n    left: 50%;\n    top: 170px;\n    margin-left: -100px;\n  }\n  .mouth .up {\n    position: relative;\n    top: -20px;\n    z-index: 1;\n  }\n  .mouth .up .lip {\n    border: 3px solid #000;\n    width: 100px;\n    height: 30px;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    position: relative;\n    position: absolute;\n    left: 50%;\n    margin-left: -50px;\n    background-color: #ffe600;\n  }\n  .mouth .up .lip.left {\n    border-radius: 0 0 0 50px;\n    transform: rotate(-15deg) translateX(-53px);\n  }\n  .mouth .up .lip.right {\n    border-radius: 0 0 50px 0;\n    transform: rotate(15deg) translateX(53px);\n  }\n  .mouth .up .lip::before {\n    content: \"\";\n    display: block;\n    width: 7px;\n    height: 30px;\n    position: absolute;\n    bottom: 0px;\n    background-color: #ffe600;\n  }\n  .mouth .up .lip.left::before {\n    right: -6px;\n  }\n  .mouth .up .lip.right::before {\n    left: -6px;\n  }\n  .mouth .down {\n    height: 180px;\n    width: 100%;\n    position: absolute;\n    top: 5px;\n    overflow: hidden;\n  }\n  .mouth .down .yuan1 {\n    border: 3px solid #000;\n    height: 1000px;\n    width: 150px;\n    position: absolute;\n    bottom: 0;\n    left: 50%;\n    margin-left: -75px;\n    border-radius: 75px/300px;\n    background-color: #9b000a;\n    overflow: hidden;\n  }\n  .mouth .down .yuan1 .yuan2 {\n    border: 1px solid red;\n    width: 200px;\n    height: 300px;\n    position: absolute;\n    bottom: -155px;\n    left: 50%;\n    margin-left: -100px;\n    background-color: #ff485f;\n    border-radius: 100px;\n  }\n  .face {\n    border: 3px solid #000;\n    width: 88px;\n    height: 88px;\n    position: absolute;\n    top: 200px;\n    left: 50%;\n    margin-left: -44px;\n    z-index: 3;\n  }\n  .face > img {\n    position: absolute;\n    left: 50%;\n    top: 50%;\n  }\n  .face.left {\n    transform: translateX(-180px);\n    background-color: #ff0000;\n    border-radius: 50%;\n  }\n  .face.left > img {\n    transform: rotateY(180deg);\n    transform-origin: 0 0;\n  }\n  .face.right {\n    transform: translateX(180px);\n    background-color: #ff0000;\n    border-radius: 50%;\n  }\n ";
 var _default = string;
 exports.default = _default;
-},{}],"test-simple.js":[function(require,module,exports) {
-"use strict";
-
-var _css = _interopRequireDefault(require("./css.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-//老前端优化后的代码
-var player = {
-  id: undefined,
-  time: 100,
-  ui: {
-    demo: document.querySelector("#demo"),
-    demo2: document.querySelector("#demo2")
-  },
-  events: {
-    "#btnPause": "pause",
-    "#btnPlay": "play",
-    "#btnSlow": "slow",
-    "#btnNormal": "normal",
-    "#btnFast": "fast"
-  },
-  n: 1,
-  init: function init() {
-    player.ui.demo.innerText = _css.default.substr(0, player.n);
-    player.ui.demo2.innerHTML = _css.default.substr(0, player.n);
-    player.bindEvents();
-    player.play();
-  },
-  bindEvents: function bindEvents() {
-    for (var key in player.events) {
-      if (player.events.hasOwnProperty(key)) {
-        var value = player.events[key]; // pause / play / slow
-
-        document.querySelector(key).onclick = player[value];
-      }
-    }
-  },
-  run: function run() {
-    player.n += 1;
-
-    if (player.n > _css.default.length) {
-      window.clearInterval(player.id);
-      return;
-    }
-
-    player.ui.demo.innerText = _css.default.substr(0, player.n);
-    player.ui.demo2.innerHTML = _css.default.substr(0, player.n);
-    player.ui.demo.scrollTop = player.ui.demo.scrollHeight;
-  },
-  play: function play() {
-    window.clearInterval(player.id);
-    player.id = setInterval(player.run, player.time);
-  },
-  pause: function pause() {
-    window.clearInterval(player.id);
-  },
-  slow: function slow() {
-    player.pause();
-    player.time = 300;
-    player.play();
-  },
-  normal: function normal() {
-    player.pause();
-    player.time = 100;
-    player.play();
-  },
-  fast: function fast() {
-    player.pause();
-    player.time = 0;
-    player.play();
-  }
-};
-player.init();
-},{"./css.js":"css.js"}],"C:/Users/DELL/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"C:/Users/DELL/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -405,5 +331,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/DELL/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","test-simple.js"], null)
-//# sourceMappingURL=/test-simple.a01aed2e.js.map
+},{}]},{},["C:/Users/DELL/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","css.js"], null)
+//# sourceMappingURL=/css.16d824bf.js.map
